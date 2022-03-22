015ed59752af122ac0cc779bea18f4c92e0f49d6
Andrew Henkel <andrew.henkel19@stjohns.edu>
2022-03-21 21:04:09 -0400

    Added test for AuthenticationInfo

---
+++ src/application/AuthenticationInfoTest.java
@@ -0,0 +1,32 @@
+import application.AuthenticationInfo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MyFirstJUnitJupiterTests {
+
+    private AuthenticationInfo authInfo = new AuthenticationInfo("student", "12345");
+
+    @Test
+    void testGetUsername() {
+        assertEquals("student", authInfo.getUsername());
+    }
+
+    @Test
+    void testSetUsername() {
+        authInfo.setUsername("newstudent");
+        assertEquals("newstudent", authInfo.getUsername());
+    }
+
+    @Test
+    void testGetPassword() {
+        assertEquals("12345", authInfo.getPassword());
+    }
+
+    @Test
+    void testSetPassword() {
+        authInfo.setPassword("67890");
+        assertEquals("67890", authInfo.getPassword());
+    }
+}
