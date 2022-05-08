d6e8aa2061fd1b932f68360be897797ec3fed511
Andrew Henkel <andrew.henkel19@stjohns.edu>
2022-05-07 21:40:23 -0400

    Added comments to helper script

--- sql_scripts/adjust_restaurants_and_events.py
+++ sql_scripts/adjust_restaurants_and_events.py
@@ -22,11 +22,14 @@ file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), FILENAME)
 with open(file_path, "r+") as file:
     contents = file.read()
 
+    # Adds statements to create (if not exists) and use the "dinnerdates" database.
     if not contents.startswith("-- Create and use dinnerdates database"):
         contents = BEGINNING + contents
 
+    # Ensures tables are only created if they don't already exist.
     contents = re.sub(CREATE_TABLE_REGEX, CREATE_TABLE_SUBST, contents, 0)
 
+    # Remove "AUTO_INCREMENT=" text.
     contents = re.sub(AUTO_INCREMENT_REGEX, "", contents, 0)
 
     file.seek(0)
